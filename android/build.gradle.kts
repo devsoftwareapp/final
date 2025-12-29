d1515247f0f6f94fceca8e406f02c2e9ba17a794
devsoftwareapp <devsoftwaremail@gmail.com>
2025-12-29 07:54:57 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,9 +2,12 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+        // Bazı durumlarda kütüphaneler gradle plugin sayfasından çekilemezse alternatif olarak ekliyoruz
+        maven { url = uri("https://plugins.gradle.org/m2/") }
     }
 }
 
+// Build dizini ayarları
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
@@ -15,8 +18,14 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
-    project.evaluationDependsOn(":app")
+    // Proje değerlendirme bağımlılığını sadece gerekli olduğunda çalışacak şekilde güvenli hale getirelim
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            project.evaluationDependsOn(":app")
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
