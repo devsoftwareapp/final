33d72cd044134d6f2457c95c3df6ebae5111c6e7
devsoftwareapp <devsoftwaremail@gmail.com>
2025-12-31 17:09:53 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,51 +1,32 @@
-import com.android.build.gradle.BaseExtension
-import org.gradle.api.tasks.Delete
-import org.gradle.api.file.Directory
-
 allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-        maven { url = uri("https://jitpack.io") }
-    }
+repositories {
+google()
+mavenCentral()
+gradlePluginPortal()
+maven { url = uri("https://jitpack.io") }
+}
 }
 
 val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-
+rootProject.layout.buildDirectory
+.dir("../../build")
+.get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 
 subprojects {
-    afterEvaluate {
-        if (project.name != "app") {
-            project.evaluationDependsOn(":app")
-        }
-    }
+afterEvaluate {
+if (project.name != "app") {
+project.evaluationDependsOn(":app")
 }
-
-/**
- * 🔴 ASIL ÇÖZÜM BURASI
- * android_intent_plus ve benzeri plugin'lerde eksik olan
- * compileSdk hatasını MERKEZDEN çözer
- */
-subprojects {
-    afterEvaluate {
-        extensions.findByName("android")?.let {
-            (it as BaseExtension).apply {
-                compileSdkVersion(34)
-            }
-        }
-    }
+}
 }
 
 tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+delete(rootProject.layout.buildDirectory)
 }
+
