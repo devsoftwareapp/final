59ff5fefbbc59acb9006bdc581c49cec2f411c5c
devsoftwareapp <devsoftwaremail@gmail.com>
2025-12-31 16:53:42 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,7 @@
+import com.android.build.gradle.BaseExtension
+import org.gradle.api.tasks.Delete
+import org.gradle.api.file.Directory
+
 allprojects {
     repositories {
         google()
@@ -11,6 +15,7 @@ val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
         .get()
+
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
@@ -22,6 +27,21 @@ subprojects {
     afterEvaluate {
         if (project.name != "app") {
             project.evaluationDependsOn(":app")
+        }
+    }
+}
+
+/**
+ * 🔴 ASIL ÇÖZÜM BURASI
+ * android_intent_plus ve benzeri plugin'lerde eksik olan
+ * compileSdk hatasını MERKEZDEN çözer
+ */
+subprojects {
+    afterEvaluate {
+        extensions.findByName("android")?.let {
+            (it as BaseExtension).apply {
+                compileSdkVersion(34)
+            }
         }
     }
 }
